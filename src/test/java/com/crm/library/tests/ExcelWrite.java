d21c33f46eebd5668349ef880bc1609145c106ee
Talha Özbabuccu <talhaoz105@gmail.com>
2023-11-15 01:06:16 +0300

    Apache POI added

---
+++ src/test/java/com/crm/library/tests/ExcelWrite.java
@@ -0,0 +1,35 @@
+package com.crm.library.tests;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelWrite {
+
+    String filepath = "SampleData.xlsx";
+
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+
+    @Test
+    public void excel_write() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(filepath);
+        workbook = new XSSFWorkbook(fileInputStream);
+        sheet = workbook.getSheet("Employees");
+
+        XSSFCell salaryCell = sheet.getRow(0).createCell(3);
+        salaryCell.setCellValue("Salary");
+
+        XSSFCell salary1 = sheet.getRow(1).createCell(3);
+        salary1.setCellValue(200000);
+
+        XSSFCell salary2 = sheet.getRow(2).createCell(3);
+        salary1.setCellValue(200000);
+    }
+
+
+}
